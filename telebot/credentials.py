58cd85d012bf80c228672b5fc3ea25bf5b8b32d1
Everton-Luiss <elssufpa@gmail.com>
2020-08-21 10:52:26 -0300

    Update credentials.py

--- telebot/credentials.py
+++ telebot/credentials.py
@@ -1,3 +1,3 @@
 bot_token = "1230428394:AAHGrYpN5YIzROkBTUf08xll_tFT7h6KZ-Q"
 bot_user_name = "bot username"
-URL = "https://bottestinstagr.herokuapp.com/"
+URL = "https://final-robo2.herokuapp.com/"
